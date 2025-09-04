0e4d8bf7171198bbbd8a8d45a6868c654b8a33fb
theAugust1st <bhote666@gmail.com>
2025-09-05 01:07:15 +0545

    feat: Implement the CRUD API calls

--- client/src/services/habitServices.ts
+++ client/src/services/habitServices.ts
@@ -18,3 +18,78 @@ export const getHabits= async ()=>{
     }
     return response.json();
 }
+export const createHabit = async ({habitName:habitName,frequency:frequency,description}:{habitName:string,frequency:'daily'| 'weekly',description:string}) =>{
+    const token = getToken();
+    if(!token){
+        throw new Error("No Authentication token found.")
+    }
+    const response = await fetch("/api/habits",{
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'Authorization':`Bearer ${token}`
+        },
+        body: JSON.stringify({habitName:habitName,frequency:frequency,description})
+    })
+    if(!response.ok){
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Failed to save new habit for now.')
+    }
+}
+export const updateHabit = async (habitID:string,{habitName,frequency,description}:{habitName:string,frequency:'daily'| 'weekly',description:string})=>{
+    const token = getToken()
+    if(!token){
+        throw new Error("NO Authentication token found.")
+    }
+    const response = await fetch(`/api/habits/${habitID}`,{
+        method:'PUT',
+        headers:{
+            'Content-Type':'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({habitName,frequency,description})
+    })
+    if(!response.ok){
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Failed to edit the habit for now.")
+    }
+    return response.json()
+}
+export const completeHabit = async(habitID:string)=>{
+    const token = getToken();
+    if(!token){
+        throw new Error("No authentication token found.")
+    }
+    const response = await fetch(`/api/habits/${habitID}/complete`,
+        {
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${token}`
+            }
+        }
+    )
+    if(!response.ok){
+        const errorData = await response.json();
+        throw new Error(errorData.message || 'Failed to complete the habit.');
+    }
+    return response.json();
+}
+
+export const deleteHabit = async(id:string)=>{
+    const token = getToken();
+    if(!token){
+        throw new Error("No Authentication token found.")
+    }
+    const response = await fetch(`/api/habits/${id}`,{
+        method:'DELETE',
+        headers:{
+            'Authorization':`Bearer ${token}`
+        }
+    })
+    if(!response.ok){
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Failed to delete the habit")
+    }
+    return response.json();
+}
