79c151007706202d25d36ea90523cb93672184d2
Bipin <iambipin77p@gmail.com>
2025-09-07 21:17:47 +0545

    feat: implement the landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/ui/Button"
+import LandingPage from "./pages/LandingPage"
 import DashBoard from "./pages/DashBoard"
 import Login from "./pages/Login"
 import { BrowserRouter,Navigate,Route,Routes} from "react-router-dom"
@@ -6,6 +7,7 @@ import ProtectedRoute from "./components/protectedRoute/ProtectedRoute"
 import Register from "./pages/Register"
 import { useAuth } from "./hooks/useAuth"
 import MainLayout from "./components/layouts/MainLayout"
+
 function App() {
   const {isLoggedIn} = useAuth();
   return(
@@ -18,13 +20,13 @@ function App() {
           <Route path="/dashboard" element={<DashBoard/>}></Route>
         </Route>
         </Route>
-        <Route path="/" element={isLoggedIn?<Navigate to="/dashboard"/>:<h1>Welcome to the ManKoSathi!</h1>}></Route>
+        <Route path="/" element={isLoggedIn?<Navigate to="/dashboard"/>:<LandingPage/>}></Route>
       </Routes>
-      <div>
+      {/* <div>
         <Button type="button"  disabled={false} variant={"secondary"}>Click Me</Button>
-      </div>
+      </div> */}
     </BrowserRouter>
   )
 }
 
-export default App
+export default App  
