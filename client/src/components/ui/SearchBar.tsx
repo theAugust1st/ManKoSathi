db5f7b423a2e83e09a8965ec07c155ecb84da79a
theAugust1st <bhote666@gmail.com>
2025-09-27 22:37:41 +0545

    fix:Responsive with ring

--- client/src/components/ui/SearchBar.tsx
+++ client/src/components/ui/SearchBar.tsx
@@ -9,14 +9,14 @@ type SearchBarProps = {
 
 export default function SearchBar({ value, onChange, placeholder }: SearchBarProps) {
   return (
-    <div className="flex items-center border rounded-lg px-3 py-2 bg-white shadow-sm border border-brand-400">
-      <Search size={18} className="text-gray-500 mr-2" />
+    <div className="flex text-sm sm:text-base md:text-normal lg:text-lg items-center border rounded-lg px-2 py-1 md:px-4 md:py-2 bg-white shadow-sm border border-brand-400 focus-within:border-brand-600 focus-within:ring-1 focus-within:ring-brand-500 focus-within:ring-offset-2">
+      <Search className="text-gray-500 mr-2 h-4 w-4 md:h-6 md:w-6" />
       <input
         type="text"
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder || "Search..."}
-        className="w-full focus:outline-none text-gray-700"
+        className="w-full focus:outline-none text-brand-800 placeholder:text-xs md:placeholder:text-base"
       />
     </div>
   );
