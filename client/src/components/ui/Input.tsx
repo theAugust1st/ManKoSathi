8aa1e87071d20008f965ca9f9a9ecc7ff614ce67
theAugust1st <bhote666@gmail.com>
2025-10-28 21:41:55 +0545

    feat: Implement show password

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -1,43 +1,86 @@
-import { type UseFormRegister, type FieldError } from "react-hook-form"
+import { useState } from "react";
+import { type UseFormRegister, type FieldError } from "react-hook-form";
 type InputProps = {
-    id:string,
-    label:string,
-    placeholder:string,
-    type?:string
-    register: UseFormRegister<any> ,
-    error?: FieldError
-    bgColor?:string
-    disabled?:boolean
-}
-function Input({id,label,placeholder, type='text',bgColor, register,error,disabled}:InputProps){
-//     return(
-//         <div className="w-full">
-//             <label htmlFor={id} className={`block text-sm font-medium ${bgColor === "white"?"":'text-brand-50'}mb-1`}>{label}</label>
-//             <input type={type} id={id} placeholder={placeholder} {...register(id)} className={`w-full border border-solid px-4 py-2 rounded-md bg-white text-brand-950 placeholder:brand-300 focus:outline-none focus:ring-2 transition-colors duration-200
-//                 ${error ?
-//                 `border-red-500 focus:border-red-500 focus:ring-red-200`:
-//                 `border-brand-300 focus:border-brand-500 focus:ring-brand-200`
-//                 }
-//                 `} />
-//                 {error && (
-//         <p className="text-red-600 text-xs mt-1">{"*" + error?.message}</p>
-//       )}
-//         </div>
-//     )
-    return(
-        <div className="w-full">
-            <label htmlFor={id} className={`block text-xs md:text-sm font-medium ${bgColor?"":'text-white'} mb-1`}>{label}</label>
-            <input type={type} id={id} placeholder={placeholder} disabled={disabled} {...register(id)} className={`w-full border border-solid text-sm md:text-base px-4 py-2 rounded-md bg-white text-brand-950 placeholder:brand-300 focus:outline-none focus:ring-2 transition-colors duration-200
-                ${error ?
-                `border-red-500 focus:border-red-500 focus:ring-red-200`:
-                `border-brand-300 focus:border-brand-500 focus:ring-brand-200`
+  id: string;
+  label: string;
+  placeholder: string;
+  type?: string;
+  register: UseFormRegister<any>;
+  error?: FieldError;
+  bgColor?: string;
+  disabled?: boolean;
+};
+function Input({
+  id,
+  label,
+  placeholder,
+  type = "text",
+  bgColor,
+  register,
+  error,
+  disabled,
+}: InputProps) {
+  //     return(
+  //         <div className="w-full">
+  //             <label htmlFor={id} className={`block text-sm font-medium ${bgColor === "white"?"":'text-brand-50'}mb-1`}>{label}</label>
+  //             <input type={type} id={id} placeholder={placeholder} {...register(id)} className={`w-full border border-solid px-4 py-2 rounded-md bg-white text-brand-950 placeholder:brand-300 focus:outline-none focus:ring-2 transition-colors duration-200
+  //                 ${error ?
+  //                 `border-red-500 focus:border-red-500 focus:ring-red-200`:
+  //                 `border-brand-300 focus:border-brand-500 focus:ring-brand-200`
+  //                 }
+  //                 `} />
+  //                 {error && (
+  //         <p className="text-red-600 text-xs mt-1">{"*" + error?.message}</p>
+  //       )}
+  //         </div>
+  //     )
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
+  const isPasswordField = type === "password";
+  const inputType = isPasswordField
+    ? isShowPassword
+      ? "text"
+      : "password"
+    : type;
+  return (
+    <div className="w-full">
+      <label
+        htmlFor={id}
+        className={`block text-xs md:text-sm font-medium ${
+          bgColor ? "" : "text-white"
+        } mb-1`}
+      >
+        {label}
+      </label>
+      <input
+        type={inputType}
+        id={id}
+        placeholder={placeholder}
+        disabled={disabled}
+        {...register(id)}
+        className={`w-full border border-solid text-sm md:text-base px-4 py-2 rounded-md bg-white text-brand-950 placeholder:brand-300 focus:outline-none focus:ring-2 transition-colors duration-200
+                ${
+                  error
+                    ? `border-red-500 focus:border-red-500 focus:ring-red-200`
+                    : `border-brand-300 focus:border-brand-500 focus:ring-brand-200`
                 }
-                `} />
-                {error && (
+                `}
+      />
+      {isPasswordField && (
+        <div className="flex items-center p-1 gap-2">
+          <span
+            onClick={() => setIsShowPassword((prev) => !prev)}
+            className="text-white cursor-pointer text-2xl"
+          >
+            {isShowPassword ? "🙈" : "👁️"}
+          </span>
+          <p className="text-brand-50 text-xs">{isShowPassword ? "hide password": "show password"}</p>
+        </div>
+      )}
+      {error && (
         <p className="text-red-600 text-xs mt-1">{"*" + error?.message}</p>
       )}
-        </div>
-    )
+    </div>
+  );
 }
 
-export default Input
+export default Input;
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,11 @@
-import Input from "../components/ui/Input"
-import Button from "../components/ui/Button"
-import { useForm, type SubmitHandler } from "react-hook-form"
-import z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
-import { useAuth } from "../hooks/useAuth"
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 const loginSchema = z.object({
   email: z.string().email({ error: "Please enter a valid email" }),
@@ -13,40 +13,43 @@ const loginSchema = z.object({
 });
 type LoginInputFields = z.infer<typeof loginSchema>;
 function Login() {
-    const {login} = useAuth();
-    const navigate = useNavigate();
-    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
-    const { register, reset, handleSubmit, formState: { errors }, } = useForm<LoginInputFields>({ resolver: zodResolver(loginSchema) });
-    const onSubmit: SubmitHandler<LoginInputFields> = async(data) => {
-      setIsSubmitting(true);
-      try{
-      const response = await fetch('/api/auth/login',{
-        method:"POST",
+  const { login } = useAuth();
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInputFields>({ resolver: zodResolver(loginSchema) });
+  const onSubmit: SubmitHandler<LoginInputFields> = async (data) => {
+    setIsSubmitting(true);
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json',
-          },
-        body: JSON.stringify(data), 
-      })
-      const result = await response.json()
-      if(!response.ok){
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      if (!response.ok) {
         console.log("Login Failed:", result.message);
-        alert(`Login Failed: ${result.message}`)
-      }
-      else{
-        console.log("Login Successful", result)
-        const {user,token} = result
-        login(user,token);
-        navigate('/dashboard')
+        alert(`Login Failed: ${result.message}`);
+      } else {
+        console.log("Login Successful", result);
+        const { user, token } = result;
+        login(user, token);
+        navigate("/dashboard");
         reset();
       }
-    }catch(err){
-      console.log("An error occured",err)
-      alert("An error occured during login")
-    }
-    finally{
+    } catch (err) {
+      console.log("An error occured", err);
+      alert("An error occured during login");
+    } finally {
       setIsSubmitting(false);
     }
-    };
+  };
   return (
     <div className="min-h-screen w-screen bg-auth-gradient flex justify-center items-center p-4">
       <div className="w-full max-w-md rounded-lg bg-black/20 backdrop-blur-sm p-8 border border-white/10 ">
@@ -62,27 +65,41 @@ function Login() {
             action="post"
             className="flex flex-col gap-6 "
           >
-              <Input
-                id="email"
-                label="Email"
-                type="email"
-                placeholder="a@b.c"
-                register={register}
-                error={errors.email}
-              ></Input>
-              <Input id="password" label="Password" placeholder="Password" type="password" register={register} error={errors.password}>
-              </Input>
-              <Button type="submit" disabled={isSubmitting}>{isSubmitting?"Logging in...":"Log In"}</Button>
+            <Input
+              id="email"
+              label="Email"
+              type="email"
+              placeholder="a@b.c"
+              register={register}
+              error={errors.email}
+            ></Input>
+            <Input
+              id="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+              register={register}
+              error={errors.password}
+            ></Input>
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting ? "Logging in..." : "Log In"}
+            </Button>
           </form>
-            <div className="center">
+          <div className="center">
             <p className="text-base text-center text-slate-400">
-              New to our side? <span onClick={()=>navigate('/register')} className="underline underline-offset-2 cursor-pointer hover:text-blue-500">SignUp</span>
+              New to our side?{" "}
+              <span
+                onClick={() => navigate("/register")}
+                className="underline underline-offset-2 cursor-pointer hover:text-blue-500"
+              >
+                SignUp
+              </span>
             </p>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
