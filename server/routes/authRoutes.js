47a55b96963bf34294be22fd51e2f0667bdf2754
theAugust1st <bhote666@gmail.com>
2025-10-30 15:10:52 +0545

    feat: Route for send otp

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {registerUser,loginUser,verifyOTP} = require('../controllers/authControllers')
+const {registerUser,loginUser,verifyOTP, sendOtp} = require('../controllers/authControllers')
 const router = express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/verifyOTP').post(verifyOTP)
+router.route('/sendOtp').post(sendOtp)
 router.route('/login').post(loginUser)
 module.exports = router
